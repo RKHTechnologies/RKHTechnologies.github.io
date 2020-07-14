e6f83e34812de6b6f5f4cd7dd6c1c6a8223e5f39
RKH Technologies <rhysh6@gmail.com>
2020-07-14 21:44:08 +0100

    Updates

---
+++ precache-manifest.4f8b84d4316b02a78cd4f0426cbae7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5911e95526ee37c5178bed0774868cd8",
+    "url": "/rkh/index.html"
+  },
+  {
+    "revision": "2036f5baf6d1e4047153",
+    "url": "/rkh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9b1dd42c197c116b816",
+    "url": "/rkh/static/js/2.44107ed2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rkh/static/js/2.44107ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2036f5baf6d1e4047153",
+    "url": "/rkh/static/js/main.4960c025.chunk.js"
+  },
+  {
+    "revision": "972e5a1db0b84eb41dcd",
+    "url": "/rkh/static/js/runtime-main.6e61d5a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rkh/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd78a373339c4042ff27c68ba8b20596.js"
+  "/rkh/precache-manifest.4f8b84d4316b02a78cd4f0426cbae7bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rkh/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
