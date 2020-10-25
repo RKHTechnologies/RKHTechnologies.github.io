7d45738b2eed37549557aae57a778627bb1d0aa5
RKH Technologies <rhysh6@gmail.com>
2020-10-25 12:39:11 +0000

    Added images etc

--- src/Shared/ImageLib.tsx
+++ src/Shared/ImageLib.tsx
@@ -1,131 +1,14 @@
-import rkh from '../img/RKHT_logo.png';
-import leaders from '../img/leaders_on_poachers.jpg';
-import bear from '../img/bear.png'
-import groupImage from '../img/desktop/group-of-scouts-in-helmets.jpg';
-import groupImageMobile from '../img/mobile/group-of-scouts-in-helmets.jpg';
-
-// Section images
-import beavers360 from '../img/beavers-singing.jpg';
-import cubs360 from '../img/cubs-marshmallow.jpg';
-import scouts360 from '../img/scouts-in-a-row.jpg';
-import explorers360 from '../img/explorers-on-rock.jpg';
-
-// Hero Images
-import highRopesHero from '../img/desktop/cub-high-ropes.jpg';
-import kayakingHero from '../img/desktop/kayaking.jpg';
-import expeditionHero from '../img/desktop/explorer-expedition.jpg';
-import beaversHero from '../img/beavers-and-leader.jpg';
-import cubsHero from '../img/cubs-group.jpg';
-import scoutsHero from '../img/scouts-life-jackets.jpg';
-  // Mobile
-import highRopesHeroMobile from '../img/mobile/cub-high-ropes.jpg';
-import kayakingHeroMobile from '../img/mobile/kayaking.jpg';
-import expeditionHeroMobile from '../img/mobile/explorer-expedition.jpg';
-import beaversHeroMobile from '../img/mobile/beavers-and-leader.jpg';
-import cubsHeroMobile from '../img/mobile/cubs-group.jpg';
-import scoutsHeroMobile from '../img/mobile/scouts-life-jackets.jpg';
-
-
-// Profile Images
-import jessica from '../img/desktop/JessicaHoward.jpg';
-import louise from '../img/desktop/LouiseDowell.jpg';
-import andy from '../img/desktop/AndyFrancis.jpg';
-import andyScouts from '../img/desktop/AndyFrancis_Scouts.jpg';
-import rhys from '../img/desktop/RhysHoward.jpg';
-import jacob from '../img/desktop/JacobIllston.jpg';
-import chris from '../img/desktop/ChrisArdington.jpg';
-import james from '../img/desktop/JamesHawthorne.jpg';
-import sarah from '../img/desktop/SarahGrady.jpg';
-import susanne from '../img/desktop/SusannePickering.jpg';
-import zoe from '../img/desktop/ZoeGamble.jpg';
-
-
-// Beavers Characters
-import characters from '../img/desktop/beavers-characters.png';
-
-// Fluer De Lis
-import FDL_Blue from '../img/FDL_Blue.png';
-import FDL_Green from '../img/FDL_Green.png';
-import FDL_Purple from '../img/FDL_Purple.png';
-import FDL_Red from '../img/FDL_Red.png';
-
+import logoSurround from '../img/logo_surround.png';
 
 
 export const ImagesDesktop = {
-  'rkh': rkh,
-  'leaders': leaders,
-  'bear': bear,
-  'groupImage': groupImage,
-  'beaversSection': beavers360,
-  'cubsSection': cubs360,
-  'scoutsSection': scouts360,
-  'explorersSection': explorers360,
-  
-  'highRopesHero': highRopesHero,
-  'kayakingHero': kayakingHero,
-  'expeditionHero': expeditionHero,
-  'beaversHero': beaversHero,
-  'cubsHero': cubsHero,
-  'scoutsHero': scoutsHero,
-
-  'jessica': jessica,
-  'louise': louise,
-  'andy': andy,
-  'andyScouts': andyScouts,
-  'rhys': rhys,
-  'jacob': jacob,
-  'chris': chris,
-  'james': james,
-  'sarah': sarah,
-  'susanne': susanne,
-  'zoe': zoe,
-
-
-  'characters': characters,
-  
-  'FDL_Blue': FDL_Blue,
-  'FDL_Green': FDL_Green,
-  'FDL_Purple': FDL_Purple,
-  'FDL_Red': FDL_Red,
+  'logoSurround': logoSurround,
 }
 
 
 
 export const ImagesMobile = {
-  'rkh': rkh,
-  'leaders': leaders,
-  'bear': bear,
-  'groupImage': groupImageMobile,
-  'beaversSection': beavers360,
-  'cubsSection': cubs360,
-  'scoutsSection': scouts360,
-  'explorersSection': explorers360,
-  
-  'highRopesHero': highRopesHeroMobile,
-  'kayakingHero': kayakingHeroMobile,
-  'expeditionHero': expeditionHeroMobile,
-  'beaversHero': beaversHeroMobile,
-  'cubsHero': cubsHeroMobile,
-  'scoutsHero': scoutsHeroMobile,
-
-  'jessica': jessica,
-  'louise': louise,
-  'andy': andy,
-  'andyScouts': andyScouts,
-  'rhys': rhys,
-  'jacob': jacob,
-  'chris': chris,
-  'james': james,
-  'sarah': sarah,
-  'susanne': susanne,
-  'zoe': zoe,
-
-  'characters': characters,
-  
-  'FDL_Blue': FDL_Blue,
-  'FDL_Green': FDL_Green,
-  'FDL_Purple': FDL_Purple,
-  'FDL_Red': FDL_Red,
+  'logoSurround': logoSurround,
 }
 
-export type imageLib = 'rkh' | 'leaders' | 'bear' | 'groupImage' | 'beaversSection' | 'cubsSection' | 'scoutsSection' | 'explorersSection' | 'highRopesHero' | 'kayakingHero' | 'expeditionHero' | 'beaversHero' | 'cubsHero' | 'scoutsHero' | 'jessica' | 'louise' | 'andy' | 'andyScouts' | 'rhys' | 'jacob' | 'chris' | 'james' | 'sarah' | 'susanne' | 'zoe' | 'characters' | 'FDL_Blue' | 'FDL_Green' | 'FDL_Purple' | 'FDL_Red';
+export type imageLib = 'logoSurround';
