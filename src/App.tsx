5d358d01fdc3c3049c7fa88620787c98d094dc58
RKH Technologies <rhysh6@gmail.com>
2020-10-25 16:16:18 +0000

    added initial logo/ background graient

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Home from './pages/Home';
 import GlobalStyle from './Shared/GlobalStyle';
 
 function App() {
   return (
     <>
+      <Home />
       <GlobalStyle />
     </>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,41 @@
 import React, { FC } from 'react';
+import styled from 'styled-components';
+import { ImagesDesktop } from '../Shared/ImageLib';
+import { colours, SharedSettings } from '../Shared/SharedStyles';
+
+const Container = styled.div`
+
+`;
+
+const MainBackground = styled.div`
+  width: 100vw;
+  height: 120vh;
+  background: rgb(37,120,65);
+  background: linear-gradient(180deg, rgba(37,120,65,1) 0%, rgba(19,60,33,1) 100%);
+`;
+
+const Logo = styled.div`
+  width: 500px;
+  height: 500px;
+  background: black;
+  position: fixed;
+  top: calc(50% - 250px);
+  left: calc(50% - 250px);
+  border-radius: 50%;
+  background: url(${ImagesDesktop['logoSurround']});
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
 
 const Home: FC = () => {
   return (
-    <>
-      RKH Technologies
-    </>
+    <Container>
+      <Logo />
+
+      <MainBackground>
+      </MainBackground>
+
+    </Container>
   );
 }
 
