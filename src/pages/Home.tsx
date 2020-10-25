910f16a5df22d08fdb965c3fcfc5105d7c1ca528
RKH Technologies <rhysh6@gmail.com>
2020-10-25 16:22:12 +0000

    Rotated home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,19 @@ import { ImagesDesktop } from '../Shared/ImageLib';
 import { colours, SharedSettings } from '../Shared/SharedStyles';
 
 const Container = styled.div`
-
+  width: 100vw;
+  height: 2000px;
+  overflow: hidden;
 `;
 
 const MainBackground = styled.div`
-  width: 100vw;
-  height: 120vh;
+  width: 120vw;
+  margin-left: -10%;
+  margin-top: -10%;
+  height: 130vh;
   background: rgb(37,120,65);
   background: linear-gradient(180deg, rgba(37,120,65,1) 0%, rgba(19,60,33,1) 100%);
+  transform: rotate(8deg);
 `;
 
 const Logo = styled.div`
@@ -25,6 +30,7 @@ const Logo = styled.div`
   background: url(${ImagesDesktop['logoSurround']});
   background-size: cover;
   background-repeat: no-repeat;
+  z-index: 1;
 `;
 
 const Home: FC = () => {
